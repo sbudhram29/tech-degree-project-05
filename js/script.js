750f5f2e5c6339f96606f779bcffdff992e02785
Sean Budhram <sbudhram@carlisle-etcetera.com>
2019-11-07 10:07:25 -0500

    fixed modal and search

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 const maxUsers = 12;
 let results = [];
 let employeesToHide = [];
-$(".modal-container")
-    .append(`    <div class="modal-container">
-</div>`)
-    .hide();
 
 $(".search-container").append(`<form action="#" method="get">
 <input type="search" id="search-input" class="search-input" placeholder="Search...">
@@ -22,19 +18,21 @@ $(() => {
     $.ajax({
         url: `https://randomuser.me/api/?nat=us&results=${maxUsers}`,
         dataType: 'json',
-        success: function (data) {
+        success: function(data) {
             results = data.results;
             for (let i = 0; i < results.length; i++) {
-                $("#gallery").append(`<div class="card" id="${i}">
-                <div class="card-img-container">
-                    <img class="card-img" src="${results[i].picture.medium}" alt="profile picture">
+                $("#gallery").append(`
+                <div class="card" id="${i}" data-id="${i}">
+                    <div class="card-img-container">
+                        <img class="card-img" src="${results[i].picture.medium}" alt="profile picture">
+                    </div>
+                    <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">${results[i].name.first + " " + results[i].name.last}</h3>
+                        <p class="card-text">${results[i].email}</p>
+                        <p class="card-text cap">${results[i].location.city}, ${results[i].location.state}</p>
+                    </div>
                 </div>
-                <div class="card-info-container">
-                    <h3 id="name" class="card-name cap">${results[i].name.first + " " + results[i].name.last}</h3>
-                    <p class="card-text">${results[i].email}</p>
-                    <p class="card-text cap">${results[i].location.city}, ${results[i].location.state}</p>
-                </div>
-            </div>`);
+                `);
             }
 
             handleClick();
@@ -66,38 +64,43 @@ const hideModal = () => {
  * Add user data to modal and add element
  */
 const showModal = (id) => {
-    id = parseInt(id, 10)
 
-    $(".modal-container").append(`
+    const user = results[id]
+    $("body").append(`
+    <div class="modal-container">
             <div class="modal">
                 <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                 <div class="modal-info-container">
-                    <img class="modal-img" src="${results[id].picture.large}" alt="profile picture">
-                    <h3 id="name" class="modal-name cap">${results[id].name.first} ${results[id].name.last}</h3>
-                    <p class="modal-text"> ${results[id].email} </p>
-                    <p class="modal-text cap">${results[id].location.city}</p>
+                    <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+                    <p class="modal-text"> ${user.email} </p>
+                    <p class="modal-text cap">${user.location.city}</p>
                     <hr>
-                    <p class="modal-text">${results[id].phone}</p>
-                    <p class="modal-text cap">${results[id].location.street}, ${results[id].location.city}, ${results[id].location.state} ${results[id].location.postcode}</p>
-                    <p class="modal-text">Birthday: ${new Date(results[id].dob.date).toLocaleDateString("en-US")}</p>
-                </div>
-            </div>
-
-            // IMPORTANT: Below is only for exceeds tasks
-            ${ (employeesToHide.length > 0)
-        ? ''
-        : `<div class="modal-btn-container">
+                    <p class="modal-text">${user.phone}</p>
+                    <p class="modal-text cap">${user.location.street.number} ${user.location.street.name}, ${user.location.city}, ${user.location.state} ${user.location.postcode}</p >
+        <p class="modal-text">Birthday: ${new Date(user.dob.date).toLocaleDateString("en-US")}</p>
+                </div >
+            </div >
+    ${
+        (employeesToHide.length > 0)
+            ? ''
+            : `<div class="modal-btn-container">
                         ${ (id > 0)
-            ? `<button type="button" id="modal-prev" class="modal-prev btn">Prev</button>`
-            : ''}
+                ? `<button type="button" id="modal-prev" class="modal-prev btn">Prev</button>`
+                : ''}
                     ${ (id < maxUsers - 1)
                 ? `<button type="button" id="modal-next" class="modal-next btn">Next</button>`
                 : ''}
-                    </div>`}
+                    </div></div>`}
 
 
-    `).show();
+`).show();
 
+    $(".modal-container").on("click", (e) => {
+        if (e.target.className === 'modal-container') {
+            hideModal()
+        }
+    });
     $("#modal-close-btn").on("click", () => hideModal());
 
     $("#modal-next").on("click", () => {
@@ -141,7 +144,10 @@ const searchEmployees = (keyword) => {
 
     for (let i = 0; i < results.length; i++) {
         let fullName = results[i].name.first + " " + results[i].name.last;
-        if (fullName.search(keyword) !== -1) {
+
+        let regex = new RegExp(`${keyword} `, 'gi')
+
+        if (fullName.search(regex) !== -1) {
             showEmployee(i);
         } else {
             employeesToHide.push(i);
@@ -152,8 +158,8 @@ const searchEmployees = (keyword) => {
 }
 
 const hideEmployee = (id) => {
-    $(`#${id}`).hide()
+    $(`#${id} `).hide()
 }
 const showEmployee = (id) => {
-    $(`#${id}`).show()
+    $(`#${id} `).show()
 }
